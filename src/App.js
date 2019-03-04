a1411b92d1cdf360d14f1a71fbe790a3365d15e0
Gabriel Militaru <gabriel.militaru@aeratechnology.com>
2019-03-04 17:39:35 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component} from "react";
+import {FusionCharts} from './deps';
 import "./App.css";
 
 class App extends Component{
---
+++ src/deps.js
@@ -0,0 +1,3 @@
+import FusionCharts from 'fusioncharts/core';
+// let FusionCharts = null;
+export { FusionCharts };
